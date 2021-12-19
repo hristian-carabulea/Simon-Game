0111be5563b893c6286070d69da21520612e2d56
Hristian Carabulea <hristian.carabulea@outlook.com>
2021-12-19 15:47:38 +0100

    Finished Game

--- app.js
+++ app.js
@@ -1,66 +1,83 @@
 
-var randomNumber = 0;
-var buttonColors = ["red", "blue", "green", "yellow"];
-var randomChosenColor = 0;
+var buttonColours = ["red", "blue", "green", "yellow"];
+
 var gamePattern = [];
+var userClickedPattern = [];
+
 var started = false;
 var level = 0;
-
-//1. Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
 
 main();
 
 function main() {
-  //1. Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
   $(document).keypress(function() {
-    console.log("main()");
     if (!started) {
-
-      //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
       $("#level-title").text("Level " + level);
       nextSequence();
       started = true;
     }
   });
+  
+  $(".btn").click(function() {
+  
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+  
+    playSound(userChosenColour);
+    flashColor(userChosenColour);
+  
+    checkAnswer(userClickedPattern.length-1);
+  });
+  
+  function checkAnswer(currentLevel) {
+  
+      if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        if (userClickedPattern.length === gamePattern.length){
+          setTimeout(function () {
+            nextSequence();
+          }, 1000);
+        }
+      } else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+  
+        setTimeout(function () {
+          $("body").removeClass("game-over");
+        }, 200);
+  
+        startOver();
+      }
+  }
+  
+  function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+  
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+  }
+  
+  function flashColor(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+      $("#" + currentColor).removeClass("pressed");
+    }, 100);
+  } // end function 
+  
+  function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+  }  // end function playSound()
+  
+  function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+  } // end function startOver()
+
 }
-
-
-function nextSequence() {
-
-  console.log("main()");
-
-  var randomNum = Math.random(); // get random number based on the list length
-  var randomNumber = (Math.floor(randomNum * 4));
-  console.log("randomNumber: " + randomNumber);
-
-  randomChosenColor = buttonColors[randomNumber]
-  console.log("randomChosenColor: " + randomChosenColor);
-
-  gamePattern.push(randomChosenColor);
-  console.log("gamePattern pushed colors: " + gamePattern);
-
-  flashColor(randomChosenColor);
-
-  makeSound(randomChosenColor); // have to click on the piece to activate sound?
-
-}
-
-function flashColor(color) {
-  console.log("color in: " + color + ".");
-  buttonAnimation(color);
-}
-
-function makeSound(color) {
-
-  var audio = new Audio("sounds/" + color + ".mp3");
-  audio.load();
-  audio.play();
-} //end function makeSound
-
-function buttonAnimation(color) {
-  console.log("animation color: " + color);
-  var activeButton = document.querySelector("." + color);
-  activeButton.classList.add("pressed");
-  setTimeout(function() { activeButton.classList.remove ("pressed"); },
-             170);
-} //end function buttonAnimation
